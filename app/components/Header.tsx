e34cbc69f889e3a2aa5713160583fe907f11baa1
Greg <gregoryminezzi@gmail.com>
2024-05-27 14:01:12 +0200

    style page tab selection

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,11 +3,26 @@ import AuthButton from "./AuthButton";
 
 export default async function Header() {
   return (
-    <nav className="w-full flex justify-between items-center px-3 border-b border-b-foreground/10">
+    <nav className="w-full flex md:flex-row flex-col-reverse justify-between items-center px-3 border-b border-b-foreground/10">
       <div className="flex gap-3">
-        <Link href="/dashboard">Home </Link>
-        <Link href="/dashboard/purchases">Purchases </Link>
-        <Link href="/dashboard/new-purchase">Add</Link>
+        <Link
+          href="/dashboard"
+          className="bg-black hover:scale-[1.1] transition rounded-tl-md rounded-tr-md px-3 text-btn-background"
+        >
+          Home{" "}
+        </Link>
+        <Link
+          href="/dashboard/purchases"
+          className="bg-black hover:scale-[1.1] transition rounded-tl-md rounded-tr-md  px-3 text-btn-background"
+        >
+          Purchases{" "}
+        </Link>
+        <Link
+          href="/dashboard/new-purchase"
+          className="bg-black hover:scale-[1.1] transition  rounded-tl-md rounded-tr-md  px-3 text-btn-background"
+        >
+          Add
+        </Link>
       </div>{" "}
       <div className="w-full max-w-4xl flex justify-end items-center p-1 text-sm">
         <AuthButton />
