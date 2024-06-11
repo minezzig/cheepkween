67075740f74b7948c450d354b73387f85cd2fadc
Greg <gregoryminezzi@gmail.com>
2024-06-11 11:57:44 +0200

    integrate dynamic image for products

--- app/dashboard/purchases/[purchase]/page.tsx
+++ app/dashboard/purchases/[purchase]/page.tsx
@@ -2,7 +2,6 @@ import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 import DeleteButton from "./DeleteButton";
 
-
 type ParamsType = {
   purchase: string;
 };
@@ -21,15 +20,38 @@ export default async function Purchase({
     .eq("id", +id)
     .single();
 
+  // ---------------------------------
+  const accessKey = process.env.NEXT_PUBLIC_ACCESS_KEY;
+  let imageUrl,photographer,photographerLink;
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/search/photos?query=${purchase.name}&client_id=${accessKey}`
+    );
+    const data = await response.json();
+    imageUrl = data.results[0].urls.thumb; // URL of the first image
+    photographer = data.results[0].user.name;
+    photographerLink = data.results[0].user.links.html;
+  } catch (error) {
+    console.error("Error fetching image:", error);
+  }
 
+  // ---------------------------------
 
   return (
     <div>
       <h1 className="text-2xl">Edit an item</h1>
-      <div className="p-8">
-        You bought <strong>{purchase.name}</strong> on{" "}
-        {purchase.purchase_date.slice(0, 10)} from {purchase.store} for a total
-        of ${purchase.price}
+      <div className="p-8 flex flex-col items-center">
+        <p>
+          You bought <strong>{purchase.name}</strong> on{" "}
+          {purchase.purchase_date.slice(0, 10)} from {purchase.store} for a total
+          of ${purchase.price}
+        </p>
+        {imageUrl ? (
+        <a href={photographerLink}><img src={imageUrl} alt={purchase.name}/><p className="text-xs">{photographer}</p></a>
+      ) : (
+        <p className="text-xs">No image available</p>
+      )}
+
         <div className="m-3 flex gap-3 w-full justify-center items-center">
           <Link
             className="py-2 px-3 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
